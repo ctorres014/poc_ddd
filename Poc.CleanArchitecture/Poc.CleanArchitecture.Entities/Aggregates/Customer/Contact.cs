90962f8ebfe5202dac459cdf78a806d49378d2b1
ctorres014 <ctorres014@gmail.com>
2022-10-12 19:35:05 -0300

    add new structure for aggregate entities andd add class customer and contact

---
+++ Poc.CleanArchitecture/Poc.CleanArchitecture.Entities/Aggregates/Customer/Contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poc.CleanArchitecture.Entities.Aggregates.Customer
+{
+    internal class Contact
+    {
+        public string City { get; set; }
+        public string Address { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+    }
+}
---
+++ Poc.CleanArchitecture/Poc.CleanArchitecture.Entities/Aggregates/Customer/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poc.CleanArchitecture.Entities.Aggregates.Customer
+{
+    public class Customer
+    {
+        public string CustomerID { get; private set; }
+        public string NameFirst { get; private set; }
+        public string NameLast { get; private set; }
+        private Contact Address { get; set; }
+
+        private Customer()
+        {
+
+        }
+    }
+}
