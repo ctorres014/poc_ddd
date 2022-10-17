8e1e1d256f9fb0d062d95c6f5e0c455cf69315ff
ctorres014 <ctorres014@gmail.com>
2022-10-17 18:10:26 -0300

    add proyect common connection factory class and implement unitofwork

--- Poc.CleanArchitecture/Poc.CleanArchitecture.Repositories/Repositories/UnitOfWork.cs
+++ Poc.CleanArchitecture/Poc.CleanArchitecture.Repositories/Repositories/UnitOfWork.cs
@@ -1,12 +1,46 @@
-﻿using System;
+﻿using Poc.CleanArchitecture.Common.Connection;
+using Poc.CleanArchitecture.Domain.Contract;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Poc.CleanArchitecture.Data.Repositories
 {
-    internal class UnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
+        private readonly IDbTransaction _dbTransaction;
+
+        public void Commit()
+        {
+            try
+            {
+                _dbTransaction.Commit();
+            }
+            catch (Exception ex)
+            {
+                _dbTransaction.Rollback();
+                throw new Exception($"Exception transaction commit, reason: { ex.Message }");
+            }
+            finally
+            {
+                _dbTransaction.Dispose();
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _dbTransaction?.Dispose();
+            }
+        }
     }
 }
