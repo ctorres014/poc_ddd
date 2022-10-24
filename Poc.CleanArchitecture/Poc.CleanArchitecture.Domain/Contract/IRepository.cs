80f26ea02a0434be1049fa955c09cd2f709e0de0
ctorres014 <ctorres014@gmail.com>
2022-10-23 22:57:24 -0300

    add generic repository and project presentation webapi

---
+++ Poc.CleanArchitecture/Poc.CleanArchitecture.Domain/Contract/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poc.CleanArchitecture.Domain.Contract
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> Add(T entity);
+        Task<T> Update(T entity);
+        Task Delete(T entity);
+        Task<IEnumerable<T>> All();
+        Task<IEnumerable<T>> FindById(string id);
+        Task<IEnumerable<T>> Search(Expression<Func<T, bool>> predicate);
+    }
+}
---
+++ Poc.CleanArchitecture/Poc.CleanArchitecture.Repositories/Repositories/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using Poc.CleanArchitecture.Domain.Contract;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poc.CleanArchitecture.Data.Repositories
+{
+    public abstract class GenericRepository<T> : IRepository<T> where T : class
+    {
+        private IDbConnection _connection;
+        public GenericRepository(IDbConnection connection) =>
+            (_connection) = (connection);
+
+        public Task<T> Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> All()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> FindById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> Search(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
